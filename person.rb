64c1b83d8968edba037a465a11bd4c32af7986cf
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-25 22:27:36 +0100

    add correct_namemethod to person class

--- person.rb
+++ person.rb
@@ -3,9 +3,13 @@ require './nameable'
 class Person < Nameable
   def intialize(_id, age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   attr_reader :id
