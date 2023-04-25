803cd7310dec4dbba393893c7f8ca5857c1ffde9
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-25 23:04:10 +0100

    add trimmer and capitalise decorators

--- decorator.rb
+++ decorator.rb
@@ -1,7 +1,25 @@
 require './nameable'
 
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def intialize(nameable)
     @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable[0, 9]
   end
 end
