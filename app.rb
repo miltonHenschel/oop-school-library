b6770a606c1adc02300cdd264b8e370580fb4dab
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-28 19:59:33 +0100

    create persons: student or teacher

--- app.rb
+++ app.rb
@@ -20,6 +20,16 @@ class App
     end
   end
 
+  def create_person(person)
+    puts 'Press 1 to create a student or 2 to create a teacher: /n'
+    option = gets.chomp
+    if option = '1'
+      create_student(person)
+    elsif option = '2'
+      create_teacher(person)
+    end
+  end
+
 app = App.new
 app.list_books
 app.list_persons
