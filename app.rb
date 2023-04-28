c513fe0d57160ef218118a3148d8f4a727b8574e
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-28 19:44:57 +0100

    list all persons

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
 class App
   def initialize
     @book_list = []
+    @person_list = []
   end
 
   def list_books
     if @book_list.length == 0
-      puts "You have no books."
+      puts "Empty!"
     else
       @book_list.each{|index, content| puts '#{index} - #{content}'}
     end
   end
 
+  def list_persons
+    if @person_list.length == 0
+      puts "Empty!"
+    else
+      @person_list.each{|index, content| puts '#{index} - #{content}'}
+    end
+  end
+
 app = App.new
 app.list_books
+app.list_persons
 end
