a12038aa50b07d769a05f9e68ee39a1653471022
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-29 19:02:09 +0100

    test, debug and fix code

--- app.rb
+++ app.rb
@@ -1,119 +1,127 @@
+require './book'
+require './classroom'
+require './person'
+require './rental'
+require './student'
+require './teacher'
+
 class App
+  attr_accessor :people, :books, :rentals
+
   def initialize
-    @book_list = []
-    @person_list = []
-    @rental_list = []
+    @books = []
+    @persons = []
+    @rentals = []
   end
 
-  def list_books(books)
-    if books.empty?
-      puts 'Oops, you have no book yet!\n'
+  def list_books
+    if @books.empty?
+      puts 'Oops, you have no book yet!'
     else
-      books.each do |book|
-        puts 'Title: #{book.title}\n'
-        puts 'Author: #{book.author}\n'
-        puts '-------------------------\n'
-      end
+      @books.each { |book| puts "[Book] Title: #{book.title}, Author: #{book.author}" }
     end
   end
 
-  def list_persons(person)
-    if persons.empty?
-      puts 'Oops, you have no person added yet!\n'
+  def list_persons
+    if @persons.empty?
+      puts 'Oops, you have no person added yet!'
     else
-      persons.each do |person|
-        puts 'No.: #{person.id}\n'
-        puts 'Name: #{person.name}\n'
-        puts 'Age: #{person.age}\n'
-        puts 'Parent permission?: #{person.parent_permission}\n'
-        puts '-------------------------\n'
-      end
-    end
-  end
-
-  def add_persons(person)
-    puts 'Press 1 to add a student or 2 to add a teacher: /n'
-    option = gets.chomp
-    if option = '1'
-      add_students(person)
-    elsif option = '2'
-      add_teachers(person)
-    end
-  end
-
-  def add_students(person)
-    puts 'Enter name: \n'
-    name = gets.chomp
-    puts 'Enter age: \n'
-    age = gets.chomp
-    puts 'Enter classroom: \n'
-    classroom = gets.chomp
-
-    student = Student.new(name, age, parent_permission: true, classroom)
-    person.push(student)
-    puts 'You successfully added a student!\n'
-  end
-
-  def add_teachers(person)
-    puts 'Enter name: \n'
-    name = gets.chomp
-    puts 'Enter age: \n'
-    age = gets.chomp
-    puts 'Enter specialization: \n'
-    specialization = gets.chomp
-
-    teacher = Teacher.new(name, age, parent_permission: true, specialization)
-    person.push(teacher)
-    puts 'You successfully added a teacher!\n'
-  end
-
-  def add_books(books)
-    puts 'Enter title: \n'
-    title = gets.chomp
-    puts 'Enter author: \n'
-    author = gets.chomp
-
-    book = Book.new(title, author)
-    books.push(book)
-    puts 'You successfully added a teacher!\n'
-  end
-
-  def add_rentals(persons, books, rentals)
-    if persons.empty?
-      puts 'Oops, no person to rent.\n'
-    elsif  books.empty?
-      puts 'Oops, no book for rent.\n'
-    else
-      puts 'Choose a person: \n'
-      persons.each{|index, person| puts '#{person.id} - #{person.id} #{person.age}'}
-      puts 'Person index: '
-      person_index = gets.chomp.to_i
-
-      puts 'Choose a book: \n'
-      books.each{|index, book| puts '#{book.id} - #{book.title}: #{person.author}'}
-      puts 'Person index: '
-      book_index = gets.chomp.to_i
-    end
-
-    rental = Rental.new(persons[person_index], books[book_index])
-    rentals.push(rental)
-    puts 'You successfully added a teacher!\n'
-  end
-
-  def list_rentals(persons, rentals)
-    if rentals.empty?
-      puts 'Oops, you have taken no rents yet.\n'
-    else
-      puts 'Choose a person by index: \n'
-      persons.each{|index, person| puts '#{person.index} - #{person.name}'}
-      puts 'Person index: '
-      person_index = gets.chomp.to_i
-      name = persons[person_index].name
-      rentals.each do |rental|
-        if rental.person.name = name
-          puts rental.rentals
+      @persons.each do |person|
+        if person.is_a? Student
+          puts "[Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}, Classroom: #{person.classroom}"
+        elsif person.is_a? Teacher
+          puts "[Teacher] Name: #{person.name}, ID: #{person.id}"
+          puts "Age: #{person.age}, Specialization: #{person.specialization}"
         end
       end
     end
+  end
+
+  def list_rentals
+    if @rentals.empty?
+      puts 'Oops, you have taken no rents yet.'
+    else
+      @rentals.each do |rental|
+        if rental.person.is_a? Student
+          puts "[Rental] Date: #{rental.date}, Title: #{rental.book.title}, Author: #{rental.book.author}"
+          puts "[Student] Name: #{rental.person.name}, ID: #{rental.person.id}"
+          puts "Age: #{rental.person.age}, Classroom: #{rental.person.classroom}"
+        elsif rental.person.is_a? Teacher
+          puts "[Rental] Date: #{rental.date}, Title: #{rental.book.title}, Author: #{rental.book.author}"
+          puts "[Teacher] Name: #{rental.person.name}, ID: #{rental.person.id}"
+          puts "Age: #{rental.person.age}, Specialization: #{rental.person.specialization}"
+        end
+      end
+    end
+  end
+
+  def add_persons
+    puts 'Press 1 to add a student or 2 to add a teacher: '
+    option = gets.chomp
+    case option
+    when '1'
+      add_students
+    when '2'
+      add_teachers
+    end
+  end
+
+  def add_students
+    puts 'Enter name: '
+    name = gets.chomp
+    puts 'Enter age: '
+    age = gets.chomp
+    puts 'Enter classroom: '
+    classroom = gets.chomp
+
+    student = Student.new(id = Random.rand(1..1000), age, classroom, name, parent_permission: true)
+    @persons.push(student)
+    puts 'You successfully added a student!'
+  end
+
+  def add_teachers
+    puts 'Enter name: '
+    name = gets.chomp
+    puts 'Enter age: '
+    age = gets.chomp
+    puts 'Enter specialization: '
+    specialization = gets.chomp
+
+    teacher = Teacher.new(id = Random.rand(1..1000), age, specialization, name, parent_permission: true)
+    @persons.push(teacher)
+    puts 'You successfully added a teacher!'
+  end
+
+  def add_books
+    puts 'Enter title: '
+    title = gets.chomp
+    puts 'Enter author: '
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'You successfully added a teacher!'
+  end
+
+  def add_rentals
+    puts 'Enter the ID of the book you want to rent: '
+    @books.each_with_index { |book, index| puts "#{index} - Title: #{book.title}, Author: #{book.author}" }
+    book_index = gets.chomp.to_i
+
+    puts 'Enter the ID of the person who wants to rent the book: '
+    @persons.each_with_index do |person, index|
+      if person.is_a? Student
+        puts "#{index} - [Student] Name: #{person.name}, ID: #{person.id}"
+        puts "Age: #{person.age}, Classroom: #{person.classroom}"
+      elsif person.is_a? Teacher
+        puts "#{index} - [Teacher] Name: #{person.name}, ID: #{person.id}"
+        puts "Age: #{person.age}, Specialization: #{person.specialization}"
+      end
+    end
+    person_index = gets.chomp.to_i
+
+    rental = Rental.new(@persons[person_index], @books[book_index])
+    @rentals.push(rental)
+    puts 'You successfully added a rental!'
   end
 end
--- main.rb
+++ main.rb
@@ -1,56 +1,49 @@
 require './app'
 
-class Main
+
+def menu
+  puts '
+  1 > List all books
+  2 > List all persons
+  3 > Add a person
+  4 > Add a book
+  5 > Add a rental
+  6 > List all rentals
+  7 > Exit
+  '
+  puts 'Please kindly choose a number: '
+end
+
+def main
+  puts 'Welcome to the School Library!'
+  choice = ''
   app = App.new
-
-  def initialize
-    @book_list = []
-    @person_list = []
-    @rental_list = []
-  end
-
-  def choice
-    puts '\nWelcome to the School Library!\n\n'
-    puts '
-    1 > List all books
-    2 > List all persons
-    3 > Add a person
-    4 > Add a book
-    5 > Add a rental
-    6 > List all rentals
-    7 > Exit
-    '
-    puts 'Please kindly choose a number: \n'
-    choice = gets.chomp
-  end
-
-  def display(choice)
+  menu
+  while choice = gets.to_i
     case choice
-    when '1'
-      list_books(@book_list)
-      choice
-    when '2'
-      list_persons(@person_list)
-      choice
-    when '3'
-      add_persons(@person_list)
-      choice
-    when '4'
-      add_books(@book_list)
-      choice
-    when '5'
-      add_rentals(@person_list, @book_list, @rental_list)
-      choice
-    end
-    when '6'
-      list_rentals(@person_list, @rental_list)
-      choice
-    end
-    when '7'
-      puts 'Thanks for using this app. Good bye!\n'
+    when 1
+      app.list_books
+      menu
+    when 2
+      app.list_persons
+      menu
+    when 3
+      app.add_persons
+      menu
+    when 4
+      app.add_books
+      menu
+    when 5
+      app.add_rentals
+      menu
+    when 6
+      app.list_rentals
+      menu
+    when 7
+      puts 'Hope you enjoyed using this app. Good bye!'
+      exit
     end
   end
 end
 
-main = Main.new
-main.choice
+main
